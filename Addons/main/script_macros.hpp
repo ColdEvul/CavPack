501fe4b2344958d32ecdff9e6d74a9e50cbb5f43
ColdEvul <andreas.brostrom.ce@gmail.com>
2016-04-19 17:40:35 +0200

    Fixed ace dependency for macros

--- Addons/main/script_macros.hpp
+++ Addons/main/script_macros.hpp
@@ -1,25 +1,6 @@
 #include "\x\cba\addons\main\script_macros_common.hpp"
 #include "\x\cba\addons\xeh\script_xeh.hpp"
+#include "\z\ace\addons\main\script_macros.hpp"
 
 // Default versioning level
 #define DEFAULT_VERSIONING_LEVEL 2
-
-#define MACRO_ADDWEAPON(WEAPON,COUNT) class _xx_##WEAPON { \
-    weapon = #WEAPON; \
-    count = COUNT; \
-}
-
-#define MACRO_ADDITEM(ITEM,COUNT) class _xx_##ITEM { \
-    name = #ITEM; \
-    count = COUNT; \
-}
-
-#define MACRO_ADDMAGAZINE(MAGAZINE,COUNT) class _xx_##MAGAZINE { \
-    magazine = #MAGAZINE; \
-    count = COUNT; \
-}
-
-#define MACRO_ADDBACKPACK(BACKPACK,COUNT) class _xx_##BACKPACK { \
-    backpack = #BACKPACK; \
-    count = COUNT; \
-}
